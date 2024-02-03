c4390682463b74daa98f0a2cc25399ea98f68100
Ольга Лосева <olgalo@list.ru>
2024-02-03 19:46:55 +0300

    Добавьте файлы проекта.

---
+++ Lab11/Equation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    struct Equation
+    {
+        double k;
+        double b;
+
+        public Equation(double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+        }
+
+        public string Root()
+        {
+            double rez = -b / k;
+            return $"Ответ: {rez}";
+        }
+    }
+}
---
+++ Lab11/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    class Program
+    {
+        //Разработать структуру для решения линейного уравнения 0=kx+b. 
+        //Коэффициенты уравнения k, b реализовать с помощью полей вещественного типа. 
+        //Для решения уравнения предусмотреть метод Root. Создать экземпляр разработанной структуры. 
+        //Осуществить использование экземпляра в программе.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите коэффициенты:");
+            Console.Write("k= ");
+            double k = Convert.ToDouble(Console.ReadLine());
+            Console.Write("b= ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            Equation eq = new Equation(k, b);
+            Console.WriteLine(eq.Root());
+            Console.ReadKey();
+        }
+    }
+}
